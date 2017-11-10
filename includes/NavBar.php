be1a858777d529302da97aa75c30288700e2bf2e
Mahfuza Shovik <mahfuzashovik@dhcp-hol-306.eduroam.cornell.edu>
2017-11-10 14:54:38 -0500

    Added includes folder

---
+++ includes/NavBar.php
@@ -0,0 +1,15 @@
+<!-- This file contains NavBar for the webpage (in header). Please check href for Home, About US and Event,
+as Prince writes these three pages. Not sure how he names each page, current href links are place holder. -->
+
+<header>
+  <nav class = "navbar">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About Us</a></li>
+      <li><a href="photos.php">Photos</a></li>
+      <li><a href="event.php">Event</a></li>
+      <li><a href = "reserve.php">Online Reservasion</a></li>
+      <li><a href = "contact.php">Contact US</a></li>
+    </ul>
+  </nav>
+</header>
---
+++ includes/footercontact.php
@@ -0,0 +1,12 @@
+<!-- This file contains location and contact for the webpage (in footer). -->
+
+<!--Info Source: http://orgsync.rso.cornell.edu/org/bowling -->
+
+<footer>
+  <hr/>
+  <h4>Contact</h4>
+  <ul class = "locTel">
+    <li>Location: 410 Thurston Ave, Ithaca, 14850 </li>
+    <li>Tel: (607)-255-5241</li>
+  </ul>
+</footer>
